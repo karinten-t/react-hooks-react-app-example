213ad6be7e2ebed4b2fcca4c4e9537a5e72a12f3
Pascal <pascalrintareseketeti@gmail.com>
2025-04-11 00:50:52 +0300

    adding react hooks example

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react'
+
+export const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+export default index;
